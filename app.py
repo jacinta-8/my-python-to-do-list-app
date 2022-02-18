5f1c6e8f83c9b0c853b2273c94560bfe761ad921
jacinta-8 <99314286+jacinta-8@users.noreply.github.com>
2022-02-18 16:12:50 +0300

    first push
    my very first one.

---
+++ app.py
@@ -0,0 +1,15 @@
+"""name = print("Jacinta Kawila")
+print("0----")
+print(" ||||")
+print('*' * 10)
+price = 20
+rating = 4.6
+print(price)
+name = input("What is your name? ")
+print('Hi ' + name)
+favorite_color = input("What is your favorite color? ")
+print(name + " likes " + favorite_color)"""
+year_of_birth = input("What is your year of birth: ")
+Age = 2019 - int(year_of_birth)
+print(Age)
+
